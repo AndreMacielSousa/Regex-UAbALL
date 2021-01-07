79d529cf5e33767dee0ba7bbd17df0db82cc4779
André Maciel Sousa <1300012@estudante.uab.pt>
2021-01-07 15:08:51 +0000

    2nd step

--- regex.js
+++ regex.js
@@ -1,41 +1,43 @@
 
-function regex(){
+function regex() {
     var string = document.getElementById("str").value;
-    var regex =document.getElementById("reg").value;
+    var regex = document.getElementById("reg").value;
     var slash = "/";
     var flag = "/";
 
-    if (document.getElementById("ignore").checked == true ){
+    if (document.getElementById("ignore").checked == true) {
         flag = flag.concat(document.getElementById("ignore").value);
     }
 
-    if (document.getElementById("global").checked == true ){
-      flag = flag.concat(document.getElementById("global").value);
+    if (document.getElementById("global").checked == true) {
+        flag = flag.concat(document.getElementById("global").value);
     }
-/*
-    if (document.getElementById("multi").checked == true ){
+
+    if (document.getElementById("multi").checked == true) {
         flag = flag.concat(document.getElementById("multi").value);
     }
-*/
+
     var regexflag = eval(slash.concat(regex).concat(flag));
-  //  console.log(regexflag);
+    //  console.log(regexflag);
     var resultadoV = regexflag.test(string);
     var resultadoE = string.match(regexflag);
+    var resultadoL = string.match(regexflag).length;
     document.getElementById("valida").innerHTML = resultadoV;
     document.getElementById("extrat").innerHTML = resultadoE;
+    document.getElementById("compri").innerHTML = resultadoL;
 }
 
 
-
+// Testes Gerais
 
 function myFunction(i) {
-    console.log(i)
-    var testStr = document.getElementById("str"+i).value;
-    //console.log(testStr);
-    var testRegex = eval(document.getElementById("reg"+i).value);
-    //console.log(testRegex);
+    var testStr = document.getElementById("str" + i).value;
+    var testRegex = eval(document.getElementById("reg" + i).value);
+    var replaceText = document.getElementById("rep" + i).value;
     var resultadoV = testRegex.test(testStr);
     var resultadoE = testStr.match(testRegex);
-    document.getElementById("valida"+i).innerHTML = resultadoV;
-    document.getElementById("extrat"+i).innerHTML = resultadoE;
+    var result = testStr.replace(testRegex, replaceText);
+    document.getElementById("valida" + i).innerHTML = resultadoV;
+    document.getElementById("extrat" + i).innerHTML = resultadoE;
+    document.getElementById("replace" + i).innerHTML = result;
 }
